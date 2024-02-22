2a6dd38ec25ff4692d9ef511ccc084ae473df753
Tanaca <jtanacar@gmail.com>
2024-02-21 23:01:11 -0300

    begin: initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    devtools: { enabled: true },
+    builder: "vite",
+    css: ['~/assets/css/main.css'],
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+});
